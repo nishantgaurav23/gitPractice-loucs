13d1fa585ac1bc38b71c3d45e1ffde724d30b3a6
nishantgaurav23 <nishantgaurav23@gmail.com>
2020-07-05 08:56:27 +0530

    Changes in app.js, gitPractice.html

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-console.log("The world is suffering from Covid -19 pandemic")
+console.log("The world is suffering from Covid -19 pandemic");
+console.log("The world is not a safe place. It is now full of chaos");
